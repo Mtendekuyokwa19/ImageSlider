4db2a6ef76f2a457c16548bfc90749d49e8389e3
MtendeKuyokwa <mtendekuyokwa19@gmail.com>
2023-12-29 13:28:09 +0200

    creating an image slideshow

--- script.js
+++ script.js
@@ -1,111 +1,23 @@
-let parent=document.querySelector('#container')
-let grid=document.querySelector('.grid')
-let noGrid=document.querySelector('.noGrid')
-let black=document.querySelector('.black')
-let red=document.querySelector('.red')
-let green=document.querySelector('.green')
-let boxNum=document.querySelector('.boxSize')
-let size;
-let measurement;
-   do{
+// eslint-disable-next-line no-unused-vars
 
-           
-         measurement=function(){
-            size=prompt("enter box sizes between 17-99")
-            
-        }
-       measurement();
-       
+const movingImages = (() => {
+  const slide = document.querySelector('.slide')
 
-   }while(!(size>16&&size<100))   
-let chosenColor=function (x){
-  
-    switch(x){
-        case 1:
-            color="black"
-            console.log ("black")
-            break;
-        case 2:
-            color="red"
-            console.log ("red")
-            break;
-        case 3:
-            color="green"
-            console.log ("green")
-            break;       
-        default:
-            color="blue"
+  function previousPictureAlgorithm () {
+    const pictures = document.querySelectorAll('img')
+    const pic = pictures[pictures.length - 1]
+    slide.prepend(pic)
+  }
 
-    }
-}
+  function nextPitureAlgorithm () {
+    const pictures = document.querySelectorAll('img')
+    const pic = pictures[0]
+    pictures[0].remove()
+    slide.appendChild(pic)
+  }
+  const rightbtn = document.querySelector('#right')
+  rightbtn.addEventListener('click', nextPitureAlgorithm)
 
-chosenColor(2)
-black.addEventListener('click',function(e){
-
-    chosenColor(1);
-
-})
-red.addEventListener('click',function(e){
-
-
-    chosenColor(2)
-})
-
-green.addEventListener('click',function(e){
-
-
-    chosenColor(3)
-})
-
-function container(){
-
- let firstInnerCointainer=document.createElement('div')
- firstInnerCointainer.classList.add('div')
-    parent.appendChild(firstInnerCointainer)
-
-console.log("first container created")
-let i=0;
-while (i<=size){
-i++
-let secondInnercontainer=document.createElement('div')
-secondInnercontainer.classList.add('divIn')
-firstInnerCointainer.appendChild(secondInnercontainer)
-let secondStyle=secondInnercontainer.style;
-
-grid.addEventListener('click',function(e){
-
-
-secondStyle.setProperty("border","solid black 0.1px")
-
-})
-noGrid.addEventListener('click',function(e){
-
-    secondStyle.setProperty("border","solid black 0px")
-    
-    })
-
-secondInnercontainer.addEventListener('mouseover',function(e){
-    
-        secondStyle.setProperty("background-color",color)
-})
-
-let erase=document.querySelector('.erase')
-erase.addEventListener('click',function(e){
-
-    secondStyle.setProperty("background-color","white")
-
-})
-
-
-}
-
-}
-
-for(i=0;i<size;i++){
-    container();
-
-
-    console.log("yandhi");
-    
-            }   
-        
+  const leftbtn = document.querySelector('#left')
+  leftbtn.addEventListener('click', previousPictureAlgorithm)
+})()
