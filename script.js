8497d5145e63c2efe6835e707bafae65a166fdec
MtendeKuyokwa <mtendekuyokwa19@gmail.com>
2023-12-30 19:18:04 +0200

    addition of the skipDot algorithm

--- script.js
+++ script.js
@@ -1,81 +1,74 @@
-// eslint-disable-next-line no-unused-vars
-/* eslint-disable */
 const movingImages = (() => {
-  const slide = document.querySelector('.slide')
+  const slide = document.querySelector(".slide");
 
-  function previousPictureAlgorithm () {
-    const pictures = document.querySelectorAll('img')
-    navgationDots.changeColorToprevious()
-    const pic = pictures[pictures.length - 1]
-    slide.prepend(pic)
-   
+  function previousPictureAlgorithm() {
+    const pictures = document.querySelectorAll("img");
+    navgationDots.changeColorToprevious();
+    const pic = pictures[pictures.length - 1];
+    slide.prepend(pic);
   }
 
-  function nextPitureAlgorithm () {
-    const pictures = document.querySelectorAll('img')
-    const pic = pictures[0]
-    pictures[0].remove()
-    slide.appendChild(pic)
+  function nextPitureAlgorithm() {
+    const pictures = document.querySelectorAll("img");
+    const pic = pictures[0];
+    pictures[0].remove();
+    slide.appendChild(pic);
     navgationDots.changeColorOfbutton();
-    
   }
-  const rightbtn = document.querySelector('#right')
-  rightbtn.addEventListener('click', nextPitureAlgorithm)
+  const rightbtn = document.querySelector("#right");
+  rightbtn.addEventListener("click", nextPitureAlgorithm);
 
-  const leftbtn = document.querySelector('#left')
-  leftbtn.addEventListener('click', previousPictureAlgorithm)
-return {nextPitureAlgorithm,previousPictureAlgorithm}
+  const leftbtn = document.querySelector("#left");
+  leftbtn.addEventListener("click", previousPictureAlgorithm);
+  return { nextPitureAlgorithm, previousPictureAlgorithm };
   // window.setInterval(nextPitureAlgorithm,4000)
-})()
+})();
 
-let navgationDots=(()=>{
+let navgationDots = (() => {
+  let allNavdots = document.querySelectorAll(".navigationButton button");
 
-  let allNavdots=document.querySelectorAll('.navigationButton button')
-
-  let position={pos:1,count:1};
+  let position = { pos: 1, count: 1 };
   function changeColorOfbutton() {
-    position.pos>3?position.pos=0:position.pos;
-    makebtnWhite()
-    allNavdots[position.pos++].style.setProperty("background-color","tomato")
-    position.count++
-   
-    
+    position.pos > 3 ? (position.pos = 0) : position.pos;
+    makebtnWhite();
+    allNavdots[position.pos++].style.setProperty("background-color", "tomato");
+    position.count++;
+
+    console.log(position.pos);
   }
 
   function changeColorToprevious() {
-    position.pos<2?position.pos=5:position.pos;
-    let pos=position.pos;
-    makebtnWhite()
-    allNavdots[position.pos-2].style.setProperty("background-color","tomato")
-  
-    position.pos=pos-1;
-    position.count-1;
-    
+    position.pos < 2 ? (position.pos = 5) : position.pos;
+    let pos = position.pos;
+    makebtnWhite();
+    allNavdots[position.pos - 2].style.setProperty(
+      "background-color",
+      "tomato",
+    );
+
+    position.pos = pos - 1;
+    position.count - 1;
   }
   function makebtnWhite() {
-    allNavdots.forEach(navdots=>{
-
-      navdots.style.setProperty("background-color","white")
-    })
-    
+    allNavdots.forEach((navdots) => {
+      navdots.style.setProperty("background-color", "white");
+    });
   }
 
- 
-     
-      
-   
-       
-   
-    
-  
+  function skipingTo(index) {
+    for (let i = 0; i <= index - position.pos; i++) {
+      movingImages.nextPitureAlgorithm();
+      console.log(index, position.pos);
+    }
+  }
 
-for (let i = 0; i < allNavdots.length; i++) {
-  allNavdots[i].addEventListener('click',function () {
-    skipingTo(i)
-    
-  })
-  
-}
+  let size = allNavdots.length + 1;
+  let counter = 1;
+  for (let i = 0; i < 4; i++) {
+    allNavdots[i].addEventListener("click", function () {
+      skipingTo(i + 1);
+    });
+  }
 
-return {changeColorOfbutton,changeColorToprevious}
-})()
+  return { changeColorOfbutton, changeColorToprevious };
+})();
