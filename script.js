6a0626bea18ac136ea2ef3696840719b2c7251a4
MtendeKuyokwa <mtendekuyokwa19@gmail.com>
2023-12-31 08:45:05 +0200

    changing the algorithim to be using scroll to view

--- script.js
+++ script.js
@@ -1,78 +1,62 @@
-/* eslint-no-console:error */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable semi */
 const movingImages = (() => {
-  const slide = document.querySelector(".slide");
+  const slide = document.querySelector('.slide');
+  const position = { pos: 0 };
+  function previousPictureAlgorithm () {
+    const pictures = document.querySelectorAll('img');
+    position.pos--;
+    position.pos === -1 ? (position.pos = 3) : position.pos;
 
-  function previousPictureAlgorithm() {
-    const pictures = document.querySelectorAll("img");
-    navgationDots.changeColorToprevious();
-    const pic = pictures[pictures.length - 1];
-    slide.prepend(pic);
-  }
-
-  function nextPitureAlgorithm() {
-    const pictures = document.querySelectorAll("img");
-    const pic = pictures[0];
-    pictures[0].remove();
-    slide.appendChild(pic);
+    pictures[position.pos].scrollIntoView();
     navgationDots.changeColorOfbutton();
   }
-  const rightbtn = document.querySelector("#right");
-  rightbtn.addEventListener("click", nextPitureAlgorithm);
 
-  const leftbtn = document.querySelector("#left");
-  leftbtn.addEventListener("click", previousPictureAlgorithm);
-  return { nextPitureAlgorithm, previousPictureAlgorithm,slide };
-  // window.setInterval(nextPitureAlgorithm,4000)
+  function nextPitureAlgorithm () {
+    const pictures = document.querySelectorAll('img');
+    position.pos++;
+    position.pos === 4 ? (position.pos = 0) : position.pos;
+    pictures[position.pos].scrollIntoView();
+    navgationDots.changeColorOfbutton();
+  }
+  const rightbtn = document.querySelector('#right');
+  rightbtn.addEventListener('click', nextPitureAlgorithm);
+
+  const leftbtn = document.querySelector('#left');
+  leftbtn.addEventListener('click', previousPictureAlgorithm);
+  window.setInterval(nextPitureAlgorithm, 4000);
+  return { nextPitureAlgorithm, previousPictureAlgorithm, slide, position };
 })();
 
-let navgationDots = (() => {
-  let allNavdots = document.querySelectorAll(".navigationButton button");
+const navgationDots = (() => {
+  const allNavdots = document.querySelectorAll('.navigationButton button');
 
-  let position = { pos: 1, count: 1 };
-  function changeColorOfbutton() {
-    position.pos > 3 ? (position.pos = 0) : position.pos;
+  function changeColorOfbutton () {
     makebtnWhite();
-    allNavdots[position.pos++].style.setProperty("background-color", "tomato");
-    position.count++;
-
-    
-  }
-
-  function changeColorToprevious() {
-    position.pos < 2 ? (position.pos = 5) : position.pos;
-    let pos = position.pos;
-    makebtnWhite();
-    allNavdots[position.pos - 2].style.setProperty(
-      "background-color",
-      "tomato",
+    allNavdots[movingImages.position.pos].style.setProperty(
+      'background-color',
+      'tomato'
     );
-
-    position.pos = pos - 1;
-    position.count - 1;
   }
-  function makebtnWhite() {
+
+  function makebtnWhite () {
     allNavdots.forEach((navdots) => {
-      navdots.style.setProperty("background-color", "white");
+      navdots.style.setProperty('background-color', 'white');
     });
   }
 
-  function skipingTo(index) {
-    const pictures = document.querySelectorAll("img");
-    
-    let tempKeeper=pictures[index];
-    pictures[index].remove()
-    movingImages.slide.prepend(tempKeeper);
-   
-    makebtnWhite();
+  function skipingTo (index) {
+    const pictures = document.querySelectorAll('img');
+    pictures[index].scrollIntoView();
+    movingImages.position.pos = index;
   }
 
-  let size = allNavdots.length + 1;
-  let counter = 1;
   for (let i = 0; i < 4; i++) {
-    allNavdots[i].addEventListener("click", function () {
+    allNavdots[i].addEventListener('click', function () {
       skipingTo(i);
+      changeColorOfbutton();
     });
   }
 
-  return { changeColorOfbutton, changeColorToprevious };
+  return { changeColorOfbutton };
 })();
